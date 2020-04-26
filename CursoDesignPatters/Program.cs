bd4edb1c9c878a569b9b640221ca68481272188e
Guilherme Golfetto <guilherme.golfetto@hotmail.com>
2020-04-26 20:17:49 -0300

    implementado padrao Chain of Resposibility

--- CursoDesignPatters/Program.cs
+++ CursoDesignPatters/Program.cs
@@ -6,15 +6,27 @@ namespace CursoDesignPatters
     {
         static void Main(string[] args)
         {
-            var iss = new ISS();
-            var icsm = new ICMS();
-            var iccc = new ICCC();
+            //var iss = new ISS();
+            //var icsm = new ICMS();
+            //var iccc = new ICCC();
 
-            Orcamento orcamento = new Orcamento(2000.0);
+            //Orcamento orcamento = new Orcamento(2000.0);
 
-            Console.WriteLine(iss.Calcula(orcamento));
-            Console.WriteLine(icsm.Calcula(orcamento));
-            Console.WriteLine(iccc.Calcula(orcamento));
+            //Console.WriteLine(iss.Calcula(orcamento));
+            //Console.WriteLine(icsm.Calcula(orcamento));
+            //Console.WriteLine(iccc.Calcula(orcamento));
+
+
+            CalculadorDeDescontos calculador = new CalculadorDeDescontos();
+
+            Orcamento orcamento = new Orcamento(500.0);
+            orcamento.AdicionaItem(new Item("CANETA", 250.0));
+            orcamento.AdicionaItem(new Item("LAPIS", 250.0));
+
+            double desconto = calculador.Calcula(orcamento);
+
+            Console.WriteLine(desconto);
+            
         }
     }
 }
--- README.md
+++ README.md
@@ -7,3 +7,8 @@ O padrão Strategy é muito útil quando temos um conjunto de algoritmos similar
 
 O Strategy nos oferece uma maneira flexível para escrever diversos algoritmos diferentes, e de passar esses algoritmos para classes clientes que precisam deles. Esses clientes desconhecem qual é o algoritmo "real" que está sendo executado, e apenas manda o algoritmo rodar. Isso faz com que o código da classe cliente fique bastante desacoplado das implementações concretas de algoritmos, possibilitando assim com que esse cliente consiga trabalhar com N diferentes algoritmos sem precisar alterar o seu código.
 
+## Padrão Chain of Responsibility
+
+O padrão Chain of Responsibility cai como uma luva quando temos uma lista de comandos a serem executados de acordo com algum cenário em específico, e sabemos também qual o próximo cenário que deve ser validado, caso o anterior não satisfaça a condição.
+
+Nesses casos, o Chain of Responsibility nos possibilita a separação de responsabilidades em classes pequenas e enxutas, e ainda provê uma maneira flexível e desacoplada de juntar esses comportamentos novamente.
